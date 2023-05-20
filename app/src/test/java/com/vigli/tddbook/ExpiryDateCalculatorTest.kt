db6cf8f6be074fe5a0929cfe6e2e235f88ee4ea6
vigli <vigli@kmong.com>
2023-05-20 22:41:55 +0900

    가장 쉬운 테스트 시작 - 한달 납부 만료 일 계산 상수로 테스트 통과

--- app/src/test/java/com/vigli/tddbook/ExpiryDateCalculatorTest.kt
+++ app/src/test/java/com/vigli/tddbook/ExpiryDateCalculatorTest.kt
@@ -1,4 +1,23 @@
 package com.vigli.tddbook
 
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.time.LocalDate
+
 class ExpiryDateCalculatorTest {
+    @Test
+    fun `만원 납부하면 한달 뒤가 만료일이 됨`() {
+        val billingDate = LocalDate.of(2019, 3, 1)
+        val payAmount = 10_000
+
+        val expiryDate = ExpiryDateCalculator().calculateExpiryDate(billingDate, payAmount)
+
+        assertEquals(LocalDate.of(2019, 4, 1), expiryDate)
+    }
+}
+
+class ExpiryDateCalculator {
+    fun calculateExpiryDate(billingDate: LocalDate, payAmount: Int): LocalDate {
+        return LocalDate.of(2019, 4, 1)
+    }
 }
