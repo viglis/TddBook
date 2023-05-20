350f61265c0ae4300fa8fa61953654f0a56257a4
vigli <vigli@kmong.com>
2023-05-20 22:52:43 +0900

    다른 예외 상황 테스트전 코드 리팩토링

--- app/src/test/java/com/vigli/tddbook/ExpiryDateCalculatorTest.kt
+++ app/src/test/java/com/vigli/tddbook/ExpiryDateCalculatorTest.kt
@@ -8,14 +8,18 @@ class ExpiryDateCalculatorTest {
     @Test
     fun `만원 납부하면 한달 뒤가 만료일이 됨`() {
         assertExpiryDate(
-            LocalDate.of(2019, 3, 1),
-            10_000,
+            PayData(
+                billingDate = LocalDate.of(2019, 3, 1),
+                payAmount = 10_000
+            ),
             LocalDate.of(2019, 4, 1)
         )
 
         assertExpiryDate(
-            LocalDate.of(2019, 5, 5),
-            10_000,
+            PayData(
+                billingDate = LocalDate.of(2019, 5, 5),
+                payAmount = 10_000
+            ),
             LocalDate.of(2019, 6, 5)
         )
     }
@@ -23,32 +27,51 @@ class ExpiryDateCalculatorTest {
     @Test
     fun `납부일과 한달 뒤 일자가 같지 않음`() {
         assertExpiryDate(
-            LocalDate.of(2019, 1, 31),
-            10_000,
+            PayData(
+                billingDate = LocalDate.of(2019, 1, 31),
+                payAmount = 10_000
+            ),
             LocalDate.of(2019, 2, 28)
         )
 
         assertExpiryDate(
-            LocalDate.of(2019, 5, 31),
-            10_000,
+            PayData(
+                billingDate = LocalDate.of(2019, 5, 31),
+                payAmount = 10_000
+            ),
             LocalDate.of(2019, 6, 30)
         )
 
         assertExpiryDate(
-            LocalDate.of(2020, 1, 31),
-            10_000,
+            PayData(
+                billingDate = LocalDate.of(2020, 1, 31),
+                payAmount = 10_000
+            ),
             LocalDate.of(2020, 2, 29)
         )
     }
 
-    private fun assertExpiryDate(billingDate: LocalDate, payAmount: Int, expectedExpiryDate: LocalDate) {
-        val realExpiryDate = ExpiryDateCalculator().calculateExpiryDate(billingDate, payAmount)
+    private fun assertExpiryDate(payData: PayData, expectedExpiryDate: LocalDate) {
+        val realExpiryDate = ExpiryDateCalculator().calculateExpiryDate(payData)
         assertEquals(expectedExpiryDate, realExpiryDate)
     }
 }
 
 class ExpiryDateCalculator {
-    fun calculateExpiryDate(billingDate: LocalDate, payAmount: Int): LocalDate {
-        return billingDate.plusMonths(1)
+    fun calculateExpiryDate(payData: PayData): LocalDate {
+        return payData.getBillingDate().plusMonths(1)
+    }
+}
+
+data class PayData(
+    private val billingDate: LocalDate,
+    private val payAmount: Int
+) {
+    fun getBillingDate(): LocalDate {
+        return billingDate
+    }
+
+    fun getPayAmount(): Int {
+        return payAmount
     }
 }
